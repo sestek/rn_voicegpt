274c0e6f6c5ec513df599a7a70d980286f010d5c
Tayfun Akgün <tayfun.akgun@sestek.com>
2023-03-07 15:10:02 +0300

    Lottiefiles is added.

--- App.tsx
+++ App.tsx
@@ -1,61 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import Lottie from 'lottie-react-native';
 
 function App(): JSX.Element {
+
+  const [record, setRecord] = useState<boolean>(false);
+  const triggerRecord = () => setRecord(old => !old);
+  useEffect(() => {
+    if (lottieRef?.current) {
+      if (record) {
+        lottieRef.current.play();
+      }
+      else{
+        lottieRef.current.reset();
+      }
+    }
+  }, [record]);
+
+  const lottieRef = useRef<Lottie>(null);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -68,30 +43,15 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text>test</Text>
+      <TouchableOpacity onPressIn={triggerRecord} onPressOut={triggerRecord}>
+        <Lottie
+          ref={lottieRef}
+          style={{ width: 200, height: 200 }}
+          source={require('./src/lottie/record.json')}
+          loop
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
